71d8605c80775876e0a4b36622f72baa55b7c81b
Andrew Yang <evil_eve_live@outlook.com>
2020-03-31 23:49:15 +0800

    add brew proxy in China to brew.sh

--- brew.sh
+++ brew.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env zsh
 
 # Install command-line tools using Homebrew.
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# brew proxy in China
+# 替换 Homebrew
+git -C "$(brew --repo)" remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+
+# 替换 Homebrew Core
+git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+
+# 替换 Homebrew Cask
+git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
+
+# 替换 Homebrew-bottles
+# 对于 bash 用户：
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.bash_profile
+source ~/.bash_profile
+# 对于 zsh 用户：
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.zshrc
+source ~/.zshrc
+
+### End of proxy in China
 
 # Make sure we’re using the latest Homebrew.
 brew update
