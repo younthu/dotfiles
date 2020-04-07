eaab3188a46f3ef36775066dd2cc8b867c74ed5a
Andrew Yang <evil_eve_live@outlook.com>
2020-04-07 11:34:54 +0800

    Update brew.sh
    brew install postgresql

--- brew.sh
+++ brew.sh
@@ -133,6 +133,9 @@ brew install fzf # 命令行模糊搜索工具，Ctrl + T开启搜索
 brew install cloc # 代码行数统计工具
 brew install socat # nc 加强版
 brew install autojump # zsh autojump, enable autojump plugin in zsh and put line into .zshrc: [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+echo "[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh" >> ~/.zshrc
+
+brew install postgresql
 
 ## Amusement
 
